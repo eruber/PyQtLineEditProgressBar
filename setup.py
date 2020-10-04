1cf301fb31da4b8f12440ae966b6c402f6b420c1
eruber <eruber@gmail.com>
2020-10-04 14:23:05 -0700

    Added long_description for sake of PyPi.org

--- setup.py
+++ setup.py
@@ -5,13 +5,21 @@ from setuptools import setup
 if sys.version_info[0] < 3:
 	sys.exit('Error: Python 3.x is required.')
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pyqtlineeditprogressbar',
-		version='0.3.2',
+		version='0.3.3',
 		description='A custom PyQt widget that places a configurable progress bar in the background of a QLineEdit widget.',
 		url='http://github.com/eruber/pyqtlineeditprogressbar',
 		download_url="https://github.com/eruber/PyQtLineEditProgressBar/archive/v0.3.0.tar.gz",
 		author='E.R. Uber',
 		author_email='eruber@gmail.com',
+		long_description=long_description,
+		long_description_content_type='text/markdown'
 		license='GPLv3',
 		packages=['pyqtlineeditprogressbar'],
 		install_requires=[

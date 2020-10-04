3d4ba8e43c95def0468d1613a40fb6d1e9d9efb8
eruber <eruber@gmail.com>
2020-10-04 14:29:06 -0700

    Updated long description content type

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyqtlineeditprogressbar',
-		version='0.3.3',
+		version='0.3.4',
 		description='A custom PyQt widget that places a configurable progress bar in the background of a QLineEdit widget.',
 		url='http://github.com/eruber/pyqtlineeditprogressbar',
 		download_url="https://github.com/eruber/PyQtLineEditProgressBar/archive/v0.3.0.tar.gz",
 		author='E.R. Uber',
 		author_email='eruber@gmail.com',
 		long_description=long_description,
-		long_description_content_type='text/markdown'
+		long_description_content_type='text/markdown',
 		license='GPLv3',
 		packages=['pyqtlineeditprogressbar'],
 		install_requires=[
